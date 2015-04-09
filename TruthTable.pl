3191aaf7ff8b0ab4959a046bef83be8488b218da
dilrukshi <madu1994@gmail.com>
2015-04-10 00:42:40 +0530

    lookup

--- TruthTable.pl
+++ TruthTable.pl
@@ -32,6 +32,22 @@ successor(A,S) :- reverse(A,R),
 
 next([0|R],[1|R]).
 next([1|R],[0|S]) :- next(R,S).                   
+true_value(N,_,_,N) :- member(N,[0,1]).
+true_value(X,Vars,A,Val) :- atom(X),
+                             lookup(X,Vars,A,Val).
+true_value(X and Y,Vars,A,Val) :- true_value(X,Vars,A,VX),
+                                   true_value(Y,Vars,A,VY),
+                                   b_and(VX,VY,Val).
+true_value(X or Y,Vars,A,Val) :-  true_value(X,Vars,A,VX),
+                                   true_value(Y,Vars,A,VY),
+                                   b_or(VX,VY,Val).
+true_value(not X,Vars,A,Val) :-   true_value(X,Vars,A,VX),
+                                   b_not(VX,Val).
+
+lookup(X,[X|_],[V|_],V).
+lookup(X,[_|Vars],[_|A],V) :- lookup(X,Vars,A,V).  
+                         
+                         
                          
                          
 tt(E) :- find_vars(E,[],V),
